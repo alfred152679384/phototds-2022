ad6593e7ae10f711aa885728d7f5a7288f007d6d
alfred152679384 <a.navarromartinez1@um.es>
2022-10-15 10:24:30 +0200

    Primer commit

---
+++ phototds/src/main/java/um/tds/phototds/App.java
@@ -0,0 +1,13 @@
+package um.tds.phototds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
